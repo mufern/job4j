6d49d313ba61e2218a6ebeb1f10f20b51f226ed3
mufern <mufern@mail.ru>
2018-10-02 22:17:06 +0300

    1.3 JavaDoc. Code Convention

---
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,29 @@
+ package package-info.java;
+
+ /**
+* Package for calculate task.
+*
+* @author Mufazalov Ernst 
+* @version $Id$
+* @since 0.1
+*/
+
+ package ru.job4j.calculate;
+ 
+ /**
+  * Class   chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+  * @author Mufazalov Ernst
+  * @since 02.10.2018
+  */
+ public class Calculate {
+	 /**
+	 * Main.
+	 * @param 1123123
+	 */
+	 public static void main(String[] args) {
+		 System.out.println("Hello World");
+	 }
+ }
+
+
+ 
