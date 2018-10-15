0d1b5cbf2d1d7ae9a7628994a9d5790256574de3
mufern <mufern@mail.ru>
2018-10-15 21:17:16 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.max;
+
+
+/**
+ * @author Mufazalov Ernst
+ * @version $Id$
+ * @since 0.1
+ */
+ public class Max {
+	 public int summation(int first, int second){
+		 return first + second;
+	 }
